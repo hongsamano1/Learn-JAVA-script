6e4e9626691333d04cc3c231f42d15ae8a2023d1
hongsamano1 <hongno0101@gmail.com>
2025-09-16 10:27:25 +0900

    변수와 데이터 타입

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ console.log("🚀 ~ usermbti:", usermbti)
 
 //username = "장동건" // 변경 불가라 오류 남
 //console.log("🚀 ~ username:", username)
+
+// 데이터 타입
+const num = 12;
+const str = "텍스트";
+const bool = true;
+//const empty = undefined;
+let empty; //null
+
+
+console.log("num", typeof num)
+console.log("str", typeof str)
+console.log("bool", typeof bool)
+console.log("🚀 ~ empty:", empty)
+
+//console.log("empty", typeof empty)
+
+
+/** null 과 undefined 의 차이점
+ * null은 개발자가 의도를 가지고 값을 비움
+ * undefined는 개발자 의도와 상관없이 값이 없는 상태
+ **/
+
+
