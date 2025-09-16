c660cde1c9af9d1ac5430a1c778d7a4d6e209500
hongsamano1 <hongno0101@gmail.com>
2025-09-16 09:57:36 +0900

    커밋 메시지

---
+++ index.js
@@ -0,0 +1,10 @@
+const username = "홍사마";// const는 상수(변할수 없는 값)
+console.log("🚀 ~ username:", username)
+let usermbti = "ENFP";// let은 변수(변할 수 있는 값)
+console.log("🚀 ~ usermbti:", usermbti)
+
+usermbti = "ISTJ" // 변경된 usermbti
+console.log("🚀 ~ usermbti:", usermbti)
+
+//username = "장동건" // 변경 불가라 오류 남
+//console.log("🚀 ~ username:", username)
